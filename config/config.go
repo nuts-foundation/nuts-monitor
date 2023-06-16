76df52ca1bb20e23ff9530fc92267b9200175bfa
Wout Slakhorst <wout.slakhorst@nedap.com>
2023-06-16 09:53:51 +0200

    revert config loading changes

--- config/config.go
+++ config/config.go
@@ -38,7 +38,7 @@ import (
 	"github.com/spf13/pflag"
 )
 
-const defaultPrefix = "NUTS"
+const defaultPrefix = "NUTS_"
 const defaultDelimiter = "."
 const configFileFlag = "configfile"
 const defaultConfigFile = "server.config.yaml"
@@ -99,18 +99,15 @@ func LoadConfig() Config {
 		log.Printf("Using default config because no file was found at: %s", configFilePath)
 	}
 
+	// load env flags, can't return error
+	_ = k.Load(envProvider(), nil)
+
 	config := defaultConfig()
 
 	// Unmarshal values of the config file into the config struct, potentially replacing default values
 	if err := k.Unmarshal("", &config); err != nil {
 		log.Fatalf("error while unmarshalling config: %v", err)
 	}
-
-	// load env flags, can't return error
-	_ = k.Load(envProvider(), nil)
-
-	// load cmd flags, without a parser, no error can be returned
-	_ = k.Load(posflag.Provider(flagset, defaultDelimiter, k), nil)
 
 	// Load the API key
 	if len(config.NutsNodeAPIKeyFile) > 0 {
@@ -139,7 +136,6 @@ func LoadConfig() Config {
 
 func loadFlagSet(args []string) *pflag.FlagSet {
 	f := pflag.NewFlagSet("config", pflag.ContinueOnError)
-	// add withmocknode flag
 	f.String(configFileFlag, defaultConfigFile, "Nuts monitor config file")
 	f.Usage = func() {
 		fmt.Println(f.FlagUsages())
